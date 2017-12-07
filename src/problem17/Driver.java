cd91e343318dc480a89bfeda0b0af55debb583e6
BlastoiseBlue <emmet@stanevich.com>
2017-12-07 13:51:06 -0500

    Started debugging the driver

--- src/problem17/Driver.java
+++ src/problem17/Driver.java
@@ -1,18 +1,45 @@
 package problem17;
 
-import java.util.StringTokenizer;
+//import EnhancedStringTokenizer;
 import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class Driver {
 	static String delimiters = ", ";
+	static String header = null;
 
 	public static int[][] Scores(Scanner s) {
-		int n = 0;
+		int numLines = 0;
 
-		String header = s.nextLine();
-		return null;
+		while (s.hasNextLine())
+			numLines++;
+		int[][] arr = new int[numLines][];
+		for (int i = 0; i < numLines; i++) {
+			String thisLine = s.nextLine();
+			String[] a = thisLine.split(",");
+			for (int j = 0; j < a.length; j++) {
+				arr[i][j] = Integer.parseInt(a[j]);
+			}
+		}
+
+		return arr;
+	}
+
+	public static void printScores(int[][] arr) {
+		System.out.println(header);
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j]);
+				if (j < arr[i].length - 1)
+					System.out.print(", ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void printScores(Scanner s) {
+		printScores(Scores(s));
 	}
 
 	public static void main(String[] args) {
@@ -23,10 +50,9 @@ public class Driver {
 			System.out.println("File not found.");
 			System.exit(0);
 		}
-		int numLines = 0;
-		// int numProducts=0;
-		while (fileIn.hasNextLine())
-			numLines++;
+		String header = fileIn.nextLine();
+		int[][] ratings=Scores(fileIn);
+		printScores(ratings);
 	}
 
 }
